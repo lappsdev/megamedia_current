0b8040b1d479d88b61b9fd80db466cc22a5b82a1
Your Name <you@example.com>
2023-07-11 22:36:06 +0000

    adjust device auth on default route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,11 @@ class HomeController < ApplicationController
     ip = request.headers['X-Forwarded-For'] || request.env['HTTP_X_FORWARDED_FOR'] || request.remote_addr
     if Device.find_by(ip: ip)
       Current.device = Device.find_by(ip: ip)
+      credential = Credential.create({ip: ip}).tap do |c|
+        c.mint_jwt! if c.errors.blank?
+      end
+      @json_web_token = credential.json_web_token
+
     else
       redirect_to sign_in_path
     end

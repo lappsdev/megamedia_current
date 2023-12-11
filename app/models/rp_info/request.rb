f280c8c09491a03a40f7b4ba243a0926fb00e439
Your Name <you@example.com>
2023-12-11 04:13:17 +0000

    add timeouts to rpinfo request

--- app/models/rp_info/request.rb
+++ app/models/rp_info/request.rb
@@ -1,7 +1,8 @@
 class RpInfo::Request
   def self.post(path, params, signature: nil, api_token: nil, api_secret: nil, use_ssl: false)
     uri = URI(path)
-    response = Net::HTTP.start(uri.host, uri.port, use_ssl: use_ssl) do |http|
+    response = Net::HTTP.start(uri.host, uri.port, use_ssl: use_ssl, read_timeout: 5,
+                                                   open_timeout: 5) do |http|
       request = Net::HTTP::Post.new(uri.to_s)
       request['Content-Type'] = 'application/json'
 
@@ -13,7 +14,8 @@ class RpInfo::Request
 
   def self.get(path, params, signature: nil, api_token: nil, api_secret: nil, use_ssl: false)
     uri = URI(path_with_params(path, params))
-    response = Net::HTTP.start(uri.host, uri.port, use_ssl: use_ssl) do |http|
+    response = Net::HTTP.start(uri.host, uri.port, use_ssl: use_ssl, read_timeout: 5,
+                                                   open_timeout: 5) do |http|
       request = Net::HTTP::Get.new(uri.to_s)
       request['Authorization'] = api_token || Zlinpay.api_token
       request['Signature'] = signature || Zlinpay.generate_signature(api_token: api_token, api_secret: api_secret)
